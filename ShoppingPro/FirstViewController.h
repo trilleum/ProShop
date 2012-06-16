247141459b6a2573f5f541ef9276d485f37a8c20
Tao Xie <txie@althoughpass-lm.(none)>
2012-06-16 08:31:19 -0700

    Initial Commit

---
+++ ShoppingPro/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  ShoppingPro
+//
+//  Created by Tao Xie on 6/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ ShoppingPro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShoppingPro
+//
+//  Created by Tao Xie on 6/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
